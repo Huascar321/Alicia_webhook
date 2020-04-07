25758fbdf6af73db340552e099f0a4da00a71ffe
BuildTools <marco752slayer@gmail.com>
2020-04-07 17:00:45 -0400

    Prueba con fulfillment

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
 const restService = express();
 
@@ -12,6 +22,10 @@ restService.use(
 );
 
 restService.use(bodyParser.json());
+
+function welcome(agent) {
+  agent.add(`Hola carambola`);
+}
 
 restService.post("/echo", function(req, res) {
   var speech =
@@ -39,4 +53,11 @@ restService.post("/echo", function(req, res) {
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
-});
+
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+})});
